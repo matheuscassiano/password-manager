e09e2760592d8d535602d1fc5a853988a751dc4e
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-05-30 18:27:08 -0300

    feat: add global styled components

--- src/Components/Styles.ts
+++ src/Components/Styles.ts
@@ -2,6 +2,25 @@ import styled from 'styled-components/native';
 
 export const ScreenContainer = styled.View`
     flex: 1;
-    padding: 16px;
+    padding: 0 30px;
     background-color: #1B262C;
+    position: relative;
+`;
+
+export const Center = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const WarningTitle = styled.Text`
+    color: white;
+    font-size: 25px;
+    text-align: center;
+    margin-top: 16px;
+`;
+
+export const WarningText = styled.Text`
+    color: white;
+    text-align: center;
 `;
