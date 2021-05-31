8097fbbbf257013a67be13fff332be468ee52df8
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-05-31 18:10:34 -0300

    feat: add a fake password service

---
+++ src/Services/password.service.ts
@@ -0,0 +1,46 @@
+const passwords = [
+    {
+        id: 1,
+        name: 'Github',
+        address: 'github.com',
+        user: 'matheuscassiano@gmail.com',
+        password: '12345678',
+    },
+    {
+        id: 2,
+        name: 'Facebook',
+        address: 'facebook.com',
+        user: 'matheuscassiano@gmail.com',
+        password: '12345678',
+    },
+    {
+        id: 3,
+        name: 'Spotify',
+        address: 'spotify.com',
+        user: 'matheuscassiano@gmail.com',
+        password: '12345678',
+    },
+    {
+        id: 4,
+        name: 'Gmail',
+        address: 'gmail.com',
+        user: 'matheuscassiano@gmail.com',
+        password: '12345678',
+    },
+    {
+        id: 5,
+        name: 'Gitlab',
+        address: 'gitlab.com',
+        user: 'matheuscassiano@gmail.com',
+        password: '12345678',
+    },
+    {
+        id: 6,
+        name: 'Medium',
+        address: 'medium.com',
+        user: 'matheuscassiano@gmail.com',
+        password: '12345678',
+    }
+];
+
+export default passwords;
