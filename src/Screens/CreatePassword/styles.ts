30e5cc6bd88ea66804c64fa19f094f11305689f3
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-01 20:07:55 -0300

    feat: add Create Password Screen styled components

---
+++ src/Screens/CreatePassword/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const SectionTitle = styled.Text`
+  color: white;
+  font-size: 22px;
+  font-family: regular;
+  margin-bottom: 10px;
+`;
+
+export const Row = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
