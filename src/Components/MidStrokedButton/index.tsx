00a1b240bad84cce77b355533287ce632a4a3aad
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-01 20:06:55 -0300

    feat: add Medium Stroked Button styled components

---
+++ src/Components/MidStrokedButton/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Button, ButtonText } from "./styles";
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+} from "@expo-google-fonts/poppins";
+import AppLoading from "expo-app-loading";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+export default function MidStrokedButton({
+  title,
+  onPress,
+}: {
+  title: string;
+  onPress?: Function;
+}) {
+  const [fontLoaded, error] = useFonts({
+    regular: Poppins_400Regular,
+    medium: Poppins_500Medium,
+  });
+
+  if (!fontLoaded) {
+    return <AppLoading />;
+  }
+  return (
+    <TouchableOpacity>
+      <Button onPress={onPress}>
+        <ButtonText>{title}</ButtonText>
+      </Button>
+    </TouchableOpacity>
+  );
+}
