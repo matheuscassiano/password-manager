ec8680ef75ee4721052f23bf56af393db49865e3
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-01 20:05:37 -0300

    feat: add Input component

---
+++ src/Components/Input/index.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import Icon from "react-native-vector-icons/Feather";
+import { Container, Label, StringInput, ClearInput } from "./styles";
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+} from "@expo-google-fonts/poppins";
+import AppLoading from "expo-app-loading";
+
+const Input: React.FC = ({ label, placeholder, type, secureTextEntry }) => {
+  const [textInput, setTextInput] = useState();
+  const [fontLoaded, error] = useFonts({
+    regular: Poppins_400Regular,
+    medium: Poppins_500Medium,
+  });
+
+  if (!fontLoaded) {
+    return <AppLoading />;
+  }
+  return (
+    <>
+      <Label>{label}</Label>
+      <Container>
+        <StringInput
+          placeholder={placeholder}
+          placeholderTextColor="white"
+          keyboardType={type}
+          secureTextEntry={secureTextEntry}
+          ref={input => { setTextInput(input as any) }}
+        />
+        <ClearInput onPress={() => (textInput as any).clear()}>
+          <Icon name="x-circle" size={18} color="#FFFFFF" />
+        </ClearInput>
+      </Container>
+    </>
+  );
+};
+
+export default Input;
