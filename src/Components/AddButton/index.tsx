08431f8ca658b580bba6b32d825684d6e62235f0
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-06-01 20:04:35 -0300

    feat: add function property to AddButton

--- src/Components/AddButton/index.tsx
+++ src/Components/AddButton/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { ButtonContainer, Button, ButtonIcon } from "./styles";
 
-export default function AddButton() {
+export default function AddButton({ onPress }) {
   return (
-    <ButtonContainer>
+    <ButtonContainer onPress={onPress}>
       <Button colors={["#3282B8", "#0F4C75"]}>
         <ButtonIcon name="plus" size={30} />
       </Button>
